878127ff000e3bc8eb6186799c739acbf993e187
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2023-10-13 19:14:10 +0700

    commit all files

---
+++ Homepage.js
@@ -0,0 +1,58 @@
+var popular = [
+    {
+        id:1,
+        name: "Kiểu tóc layer ngắn",
+        price: "750.000 VĐN",
+        img: "https://cdn.voh.com.vn/voh/Image/2020/12/29/toc-tia-layer-cho-mat-tron-voh-1.jpg"
+    },
+    {
+        id:2,
+        name: "Kiểu tóc layer lượn sóng dài",
+        price: "1.000.000 VĐN",
+        img: "https://img5.thuthuatphanmem.vn/uploads/2021/11/10/mau-toc-xoan-loi-ngang-vai-mai-dai_014707077.jpg"
+    },
+    {
+        id:3,
+        name: "Kiểu tóc layer lượn sóng dài",
+        price: "1.000.000 VĐN",
+        img: "https://img2.thuthuatphanmem.vn/uploads/2019/04/27/kieu-toc-xoan-song-nuoc-ngan_084307284.jpg"
+    },
+    {
+        id:3,
+        name: "Kiểu tóc layer lượn sóng dài",
+        price: "1.000.000 VĐN",
+        img: "https://top.chon.vn/wp-content/uploads/2021/02/toc-layer-nu-ngan-mat-trai-xoan.jpg"
+    }
+];
+
+var women = [
+    {
+        id:1,
+        name: "Kiểu tóc ngắn cúp đuôi",
+        price: "500.000 VĐN",
+        img: "https://top.chon.vn/wp-content/uploads/2021/02/toc-layer-nu-ngan-mat-trai-xoan.jpg"
+    },
+    {
+        id:2,
+        name: "Kiểu tóc Tomboy",
+        price: "550.000 VĐN",
+        img: "https://top.chon.vn/wp-content/uploads/2021/02/toc-layer-nu-ngan-mat-trai-xoan.jpg"
+    },
+    {
+        id:3,
+        name: "Kiểu tóc ngắn mái thưa",
+        price: "1.000.000 VĐN",
+        img: "https://top.chon.vn/wp-content/uploads/2021/02/toc-layer-nu-ngan-mat-trai-xoan.jpg"
+    },
+    {
+        id:5,
+        name: "Kiểu tóc ngắn mái thưa",
+        price: "800.000 VĐN",
+        img: "https://top.chon.vn/wp-content/uploads/2021/02/toc-layer-nu-ngan-mat-trai-xoan.jpg"
+    },
+
+]
+
+function listModelHair(){
+    
+}
